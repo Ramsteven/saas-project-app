797785388bdb0ed1ef64117fd3e7eb02f46776e0
ramsteven <ramsteven@gmail.com>
2021-03-19 15:13:30 -0500

    deleten character garbage inside enviroment productio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "saas-project-davi.herokuapp.com", :protocol => "https" }| w
+  config.action_mailer.default_url_options = { :host => "saas-project-davi.herokuapp.com", :protocol => "https" }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
