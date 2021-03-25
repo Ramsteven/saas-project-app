5a638da5cf97e88af5cbdc49383121b79f43fa23
ramsteven <ramsteven@gmail.com>
2021-03-25 14:13:58 -0500

    adding card_token to payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -11,7 +11,7 @@ class Payment < ActiveRecord::Base
   end
 
   def process_payment
-    customer = Stripe::Customer.create email: email, card: token
+    customer = Stripe::Customer.create email: email, card: card_token
 
     Stripe::Charge.create customer: customer.id,
                           amount: 1000,
