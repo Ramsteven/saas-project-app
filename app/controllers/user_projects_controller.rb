da0d169ea9224255034358fffa679ea0014dbc78
ramsteven <ramsteven@gmail.com>
2021-03-24 20:37:41 -0500

    adding usercontroller fix

--- app/controllers/user_projects_controller.rb
+++ app/controllers/user_projects_controller.rb
@@ -1,12 +1,14 @@
 class UserProjectsController < ApplicationController
-  before_action :set_user_project, only: %i[ show edit update destroy ]
+  before_action :set_user_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /user_projects or /user_projects.json
+  # GET /user_projects
+  # GET /user_projects.json
   def index
     @user_projects = UserProject.all
   end
 
-  # GET /user_projects/1 or /user_projects/1.json
+  # GET /user_projects/1
+  # GET /user_projects/1.json
   def show
   end
 
@@ -19,39 +21,44 @@ class UserProjectsController < ApplicationController
   def edit
   end
 
-  # POST /user_projects or /user_projects.json
+  # POST /user_projects
+  # POST /user_projects.json
   def create
     @user_project = UserProject.new(user_project_params)
 
     respond_to do |format|
       if @user_project.save
-        format.html { redirect_to @user_project, notice: "User project was successfully created." }
+        format.html { redirect_to @user_project, notice: 'User project was successfully created.' }
         format.json { render :show, status: :created, location: @user_project }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @user_project.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /user_projects/1 or /user_projects/1.json
+  # PATCH/PUT /user_projects/1
+  # PATCH/PUT /user_projects/1.json
   def update
     respond_to do |format|
       if @user_project.update(user_project_params)
-        format.html { redirect_to @user_project, notice: "User project was successfully updated." }
+        format.html { redirect_to @user_project, notice: 'User project was successfully updated.' }
         format.json { render :show, status: :ok, location: @user_project }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @user_project.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /user_projects/1 or /user_projects/1.json
+  # DELETE /user_projects/1
+  # DELETE /user_projects/1.json
   def destroy
     @user_project.destroy
     respond_to do |format|
-      format.html { redirect_to users_tenant_project_url(id: @user_project.project_id, @tenant_id: @user_project.project.tenant_id), notice: "User was successfully removed from the project." }
+      format.html { redirect_to users_tenant_project_url(id: @user_project.project_id,
+        tenant_id: @user_project.project.tenant_id),
+        notice: 'User was successfully removed from the project' }
       format.json { head :no_content }
     end
   end
@@ -62,7 +69,7 @@ class UserProjectsController < ApplicationController
       @user_project = UserProject.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # Never trust parameters from the scary internet, only allow the white list through.
     def user_project_params
       params.require(:user_project).permit(:project_id, :user_id)
     end
